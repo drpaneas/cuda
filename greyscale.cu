55bdf29105f6312f16143a36d02a7603e1408d3f
Panos Georgiadis <drpaneas@gmail.com>
2013-04-18 16:48:19 +0300

    Update greyscale.cu
    // Color to Greyscale Conversion
    
    //A common way to represent color images is known as RGBA - the color
    //is specified by how much Red, Grean and Blue is in it.
    //The 'A' stands for Alpha and is used for transparency, it will be
    //ignored in this homework.
    
    //Each channel Red, Blue, Green and Alpha is represented by one byte.
    //Since we are using one byte for each color there are 256 different
    //possible values for each color.  This means we use 4 bytes per pixel.
    
    //Greyscale images are represented by a single intensity value per pixel
    //which is one byte in size.
    
    //To convert an image from color to grayscale one simple method is to
    //set the intensity to the average of the RGB channels.  But we will
    //use a more sophisticated method that takes into account how the eye 
    //perceives color and weights the channels unequally.
    
    //The eye responds most strongly to green followed by red and then blue.
    //The NTSC (National Television System Committee) recommends the following
    //formula for color to greyscale conversion:
    
    //I = .299f * R + .587f * G + .114f * B
    
    //Notice the trailing f's on the numbers which indicate that they are 
    //single precision floating point constants and not double precision
    //constants.

--- greyscale.cu
+++ greyscale.cu
@@ -1,1 +1,32 @@
 #include <math.h>
+#include "reference_calc.cpp"
+#include "utils.h"
+#include <stdio.h>
+
+__global__
+void rgba_to_greyscale(const uchar4* const rgbaImage,
+                       unsigned char* const greyImage,
+                       int numRows, int numCols)
+{
+  int index_x = blockIdx.x * blockDim.x + threadIdx.x;
+  int index_y = blockIdx.y * blockDim.y + threadIdx.y;
+
+  // map the two 2D indices to a single linear, 1D index
+  int grid_width = gridDim.x * blockDim.x;
+  int index = index_y * grid_width + index_x;
+
+  // write out the result
+  greyImage[index] =  .299f * rgbaImage[index].x + .587f * rgbaImage[index].y + .114f * rgbaImage[index].z;
+  
+}
+
+void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_rgbaImage,
+                            unsigned char* const d_greyImage, size_t numRows, size_t numCols)
+{
+  const int thread = 16;
+  const dim3 blockSize( thread, thread, 1);
+  const dim3 gridSize( ceil(numRows/(float)thread), ceil(numCols/(float)thread), 1);
+  rgba_to_greyscale<<<gridSize, blockSize>>>(d_rgbaImage, d_greyImage, numRows, numCols);
+  
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+}
